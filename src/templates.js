d546557f0d33f4207c4297115912744b50ea8862
thajucp123 <thajucp123@gmail.com>
2025-12-06 15:32:36 +0530

    added one more bg design for poster

--- src/templates.js
+++ src/templates.js
@@ -4,6 +4,7 @@ const posterColors = [
   { name: "Green", color: "#2ecc71", src: "/templates/template-three.jpg" },
   { name: "Teal", color: "#1abc9c", src: "/templates/template-four.jpg" },
   { name: "Purple", color: "#9b59b6", src: "/templates/template-five.jpg" },
+   {name: "HotPink", color: "#FF69B4", src:"/templates/template-six.jpg"}
 ];
 
 export default posterColors;
